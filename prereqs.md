0b9faed930cf756275e3845d95d5dc0feec87e5a
samarthsb4real <samarthbhadane23@gmail.com>
2024-10-12 10:12:59 +0530

    readme setup

---
+++ prereqs.md
@@ -0,0 +1,9 @@
+## Tech Stack
+1. node.js
+2. Vite
+3. 
+
+#### packages to be installed:
+node.js
+
+`npm install -g npm@10.9.0`
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
 /* https://www.youtube.com/watch?v=j4jtjk_1wcQ&list=PLCnmpqh8sKKynYSJqrrC0nWwPB3OVf5lt&index=1 */
 
+class Renderer{
+    
+    private context!: GPUCanvasContext;
+
+    public async initialize(){
+        const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    }
+}
